e335cb4c20d540a2525e1da96fd3e9c9331c4924
zhao wen qiang <13143410521@163.com>
2018-04-01 22:16:51 +0800

    添加验证md5

--- test/md5.test.js
+++ test/md5.test.js
@@ -2,11 +2,23 @@ const md5 = require('../index')
 const expect = require('chai').expect
     
 describe('md5.js', () => {
-    it('md5', async () => {
+    it('sign', async () => {
         const md5_value = '202cb962ac59075b964b07152d234b70'
         const res = await md5.sign('123')
         console.log(res)
         expect(res).to.be.a('string')
         expect(res).to.equal(md5_value)
+    })
+    it('verify true', async () => {
+        const md5_value = '202cb962ac59075b964b07152d234b70'
+        const res = await md5.verify('123', md5_value)
+        console.log(res)
+        expect(res).to.equal(true)
+    })
+    it('verify false', async () => {
+        const md5_value = '202cb962ac59075b964b07152d234b701'
+        const res = await md5.verify('123', md5_value)
+        console.log(res)
+        expect(res).to.equal(false)
     })
 });
